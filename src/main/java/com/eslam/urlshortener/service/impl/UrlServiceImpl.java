959995d46bdc691c5516a6ffb7236cd31fb4aac2
eslamfawzy72 <eslamfawzy765@gmail.com>
2026-07-13 10:21:31 +0300

    feat: finish implementation of url service using redis

--- src/main/java/com/eslam/urlshortener/service/impl/UrlServiceImpl.java
+++ src/main/java/com/eslam/urlshortener/service/impl/UrlServiceImpl.java
@@ -6,21 +6,24 @@ import com.eslam.urlshortener.entity.UrlMapping;
 import com.eslam.urlshortener.service.UrlService;
 import lombok.*;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import com.eslam.urlshortener.repository.UrlRepository;
 
 import java.security.SecureRandom;
+import java.time.Duration;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UrlServiceImpl implements UrlService {
+
     private final UrlRepository urlRepo;
+    private final RedisTemplate<String,Object> redisTemplate;
+    //constants
     private static final String CHARACTERS =
             "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
     private static final int SHORT_CODE_LENGTH = 6;
-
     private static final SecureRandom RANDOM = new SecureRandom();
 
     @Override
@@ -38,15 +41,33 @@ public class UrlServiceImpl implements UrlService {
     }
     @Override
     public UrlResponse getUrl(String shortCode) {
-        return null;
+        //check if shortcode in redis get it
+        Object cached = redisTemplate.opsForValue().get(shortCode);
+        if(cached != null){
+            return (UrlResponse) cached;
+        }
+        //else get it from db and set it in redis
+        UrlMapping urlMapping = urlRepo.findByShortCode(shortCode)
+                .orElseThrow(() ->
+                        new RuntimeException("Short code not found"));
+        UrlResponse urlResponse = mapToResponse(urlMapping);
+        redisTemplate
+                .opsForValue()
+                .set(shortCode, urlResponse, Duration.ofHours(1));
+        return urlResponse;
+
     }
 
     @Override
     public List<UrlResponse> getAllUrls() {
-        return List.of();
+        return urlRepo.findAll()
+                .stream()
+                .map(this::mapToResponse)
+                .toList();
     }
 
-    //helper function
+    //helper functions
+
     private String generateShortCode() {
 
         StringBuilder shortCode = new StringBuilder();
